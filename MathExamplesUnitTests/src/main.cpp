db73ae6e09d5e8ddd250816ecdf48da4bd11fb83
alex-t <alex-t@tiger.lan>
2017-03-19 15:30:09 +0300

    unit tests

--- MathExamplesUnitTests/src/main.cpp
+++ MathExamplesUnitTests/src/main.cpp
@@ -19,4 +19,29 @@ SCENARIO("Cards works fine", "[cards]") {
 		REQUIRE(rf_checker.Test(rf_hand4).success == false);
 		REQUIRE(!rf_checker.Test(rf_hand4).success);
 	}
+
+	GIVEN("Straight Flush check")
+	{
+		StraightFlush sf_checker;
+
+		Hand sf_hand1 = { "6D", "5D", "3D", "4D", "2D" };
+		Hand sf_hand2 = { "QH", "JH", "TH", "8H", "9H" };
+
+		REQUIRE(sf_checker.Test(sf_hand1).success);
+		REQUIRE(sf_checker.Test(sf_hand2).success);
+
+		Hand sf_hand3 = { "QH", "JH", "TS", "8H", "9H" };
+		REQUIRE(!sf_checker.Test(sf_hand3).success);
+	}
+
+	GIVEN("Four of a kind poker result")
+	{
+		FourOfaKind fk_checker;
+
+		Hand fk_hand1 = { "6D", "6S", "3D", "6H", "6C" };
+		Hand fk_hand2 = { "AH", "AD", "TH", "AC", "AS" };
+
+		REQUIRE(fk_checker.Test(fk_hand1).success);
+		REQUIRE(fk_checker.Test(fk_hand2).success);
+	}
 }
